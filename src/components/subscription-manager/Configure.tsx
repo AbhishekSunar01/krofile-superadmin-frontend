9ff1294d7d760530042b6e4e839805aa82b0d978
Nitya Shahi <a8hishek01@gmail.com>
2024-08-20 18:53:01 +0545

    checkpoint

--- src/components/subscription-manager/Configure.tsx
+++ src/components/subscription-manager/Configure.tsx
@@ -1,8 +1,23 @@
 export default function Configure() {
   return (
-    <div>
-      <h1>Configure</h1>
-      <p>Configure your subscription here.</p>
+    <div className="flex flex-col gap-6">
+      <div>
+        <div className="font-medium">Grow with Referrals & Free Trials</div>
+        <div className="muted">
+          Earn rewards by referring new users and offer extended trial periods
+          to help them explore Krofile.
+        </div>
+      </div>
+      <div>
+        <form className="flex"></form>
+      </div>
+      <div>
+        <div className="font-medium">Configure Your Plan</div>
+        <div className="muted">
+          Adjust your subscription to meet your needs. Choose the features that
+          work for you.
+        </div>
+      </div>
     </div>
   );
 }
