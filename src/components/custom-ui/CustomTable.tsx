f3a057899dbf3b5d09056564e824c1b5a579172b
abhishek <a8hishek01@gmail.com>
2024-09-16 12:25:55 +0545

    feat: eagles view

---
+++ src/components/custom-ui/CustomTable.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableRow,
+} from "../../components/ui/table";
+import { Card } from "../../components/ui/card";
+import growth from "../../assets/svg/growth.svg";
+import clsx from "clsx";
+
+interface TableProps {
+  data: Record<string, any>[];
+  title: string;
+  type: string;
+  width?: string;
+}
+
+const CustomTable: React.FC<TableProps> = ({ data, title, type, width }) => {
+  const industry = [
+    { key: "_id", header: "SL No" },
+    { key: "industryType", header: "Industry Type" },
+    { key: "count", header: "Count" },
+    { key: "ratio", header: "Ratio" },
+  ];
+  const subscribers = [
+    { key: "_id", header: "SL No" },
+    { key: "businessName", header: "Business Name" },
+    { key: "date", header: "Reg. Date" },
+    { key: "plan", header: "Subs. Plan" },
+  ];
+  const columns = type === "industry" ? industry : subscribers;
+
+  const totalData = React.useMemo(
+    () => data.reduce((acc, curr) => acc + curr.count, 0),
+    [data]
+  );
+
+  return (
+    <Card className={clsx(`flex flex-col`, width ? `w-[${width}]` : "w-full")}>
+      {" "}
+      <div className="flex flex-1 flex-col justify-center gap-1 px-6 pt-5">
+        <div className="font-normal text-sm">{title}</div>
+        <div className="flex items-center gap-x-2">
+          <span className="text-base font-semibold leading-none">
+            {totalData.toLocaleString()}{" "}
+          </span>{" "}
+          <span className="text-xs flex items-center text-primary gap-1 ">
+            {" "}
+            <img
+              src={growth}
+              alt="growth"
+              className="bg-accentGreen p-1 rounded-full"
+            />{" "}
+            <span className="text-accentGreen">26%</span>
+            vs previous period
+          </span>
+        </div>
+      </div>
+      <div className="px-5 py-2">
+        {" "}
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {columns.map((column) => (
+                <th
+                  key={column.key}
+                  className="text-sm font-medium text-left pl-4 py-3 mb-[18px]"
+                >
+                  {column.header}
+                </th>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {columns.map((column) => (
+                  <td
+                    key={`${rowIndex}-${column.key}`}
+                    className=" text-left pl-5   py-3
+                 border-0"
+                  >
+                    {row[column.key]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </Card>
+  );
+};
+
+export default CustomTable;
--- src/pages/EagleView.tsx
+++ src/pages/EagleView.tsx
@@ -1,12 +1,85 @@
 import PageLayout from "../layout/PageLayout";
+import ActiveSubscribersDataJson from "../json/dummyData/activeSubscribersData.json";
+import activeUserGrowthChartDataJson from "../json/dummyData/activeUserGrowthChartData.json";
+import { ChartConfig } from "../components/ui/chart";
+import ReportCard from "../components/reports/ReportCard";
+import formatNumberWithCommas from "../utils/formatNumberWithComma";
+import ReportAreaChart from "../components/reports/AreaChart";
+import ReportTable from "../components/reports/ReportTable";
+
+interface IChartData {
+  date: string;
+  [key: string]: any; // This allows for any number of additional properties with any type
+}
 
 export default function EagleView() {
+  const activeUserGrowthChartData: IChartData[] =
+    activeUserGrowthChartDataJson.chartData;
+
+  const ActiveSubscribersData: Record<string, any>[] =
+    ActiveSubscribersDataJson.data;
+  const activeUserChartLabels: string[] = ["Count"];
+
+  const activeUserGrowthChartConfig = {
+    count: {
+      label: "Count",
+      color: "hsl(var(--chart-6))",
+    },
+  } satisfies ChartConfig;
+
+  const findTotal = (data: IChartData[]): number => {
+    const total = data.reduce((acc, curVal) => {
+      const count = curVal.count || 0; // Handle cases where count might be undefined
+      return acc + count;
+    }, 0); // Initialize the accumulator to 0
+    return total;
+  };
   return (
     <PageLayout
       title="EagleView"
       description="Eagle's View integrates visitor data from all businesses into a single, consolidated view. Monitor overall trends, enhance strategies, and make informed decisions with this powerful tool, driving growth and efficiency toward future success."
     >
-      EagleView
+      <div className="grid grid-cols-4 gap-4">
+        <ReportCard
+          cardTitle="Active Users Growth Chart"
+          cardLink="/reports"
+          growthPercentage={
+            activeUserGrowthChartDataJson.growthPercentage || "0"
+          }
+          total={
+            formatNumberWithCommas(findTotal(activeUserGrowthChartData)) || 0
+          }
+          childrenComponent={
+            <ReportAreaChart
+              chartConfig={activeUserGrowthChartConfig}
+              chartData={activeUserGrowthChartData}
+              XAxisDataKey={"date"}
+              YAxisDataKey={"count"}
+              areaType="natural"
+              chartLabels={activeUserChartLabels}
+              tickFormatter={(value) => value / 1000 + "k"}
+              gradientColors={{
+                startColor: "#22D1EE66",
+                endColor: "#85EEFF4D",
+              }}
+              strokeColor="#22D1EE"
+            />
+          }
+        />
+
+        <ReportCard
+          cardTitle="Active Subscribers"
+          cardLink="/reports"
+          growthPercentage={ActiveSubscribersDataJson.growthPercentage || "0"}
+          total={formatNumberWithCommas(ActiveSubscribersData.length) || 0}
+          childrenComponent={
+            <ReportTable
+              data={ActiveSubscribersData}
+              headings={["SL No", "Business Name", "Reg. Date", "Subs. Plan"]}
+            />
+          }
+        />
+      </div>
     </PageLayout>
   );
 }
