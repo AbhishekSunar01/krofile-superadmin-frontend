39bcb43d7f1bd976c2d48dee20cb74cf86955504
abhishek <a8hishek01@gmail.com>
2024-10-04 22:28:01 +0545

    feat: dialog created for editing single plan

--- src/components/subscription-manager/EditPlanDialog.tsx
+++ src/components/subscription-manager/EditPlanDialog.tsx
@@ -8,41 +8,102 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "../ui/dialog";
-import { Input } from "../ui/input";
-import { Label } from "../ui/label";
 import editPen from "../../assets/svg/editIcon.svg";
+import { z } from "zod";
+import { useForm, FormProvider } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { Input } from "../ui/input";
+import { PlanDetails } from "../../types/subscriptionManagementTypes";
 
-export default function EditPlanDialog() {
+const formSchema = z.object({
+  title: z.string().min(2).max(50),
+  initialPrice: z.number().min(0),
+  discount: z.number().min(0),
+  contactUs: z.boolean(),
+});
+
+interface EditPlanDialogProps {
+  plan: PlanDetails;
+}
+
+export default function EditPlanDialog({ plan }: EditPlanDialogProps) {
+  const planId = plan._id;
+  console.log(planId);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      initialPrice: 0,
+      discount: 0,
+      contactUs: false,
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+    // Handle form submission logic here
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <img src={editPen} className="w-5 h-5" />
-        {/* <Button variant="outline">Edit Profile</Button> */}
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[625px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Add New Plan</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Offer more flexibility with a newly added subscription plan.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+
+        <FormProvider {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter title" {...field} />
+                  </FormControl>
+                  {/* <FormMessage /> */}
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="initialPrice"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Initial Price</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter initial price" {...field} />
+                  </FormControl>
+                  {/* <FormMessage /> */}
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="discount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Discount</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter discount" {...field} />
+                  </FormControl>
+                  {/* <FormMessage /> */}
+                </FormItem>
+              )}
+            />
+            <DialogFooter className="w-full flex">
+              <Button type="submit">Submit</Button>
+            </DialogFooter>
+          </form>
+        </FormProvider>
       </DialogContent>
     </Dialog>
   );
